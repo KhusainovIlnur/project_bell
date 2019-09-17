6eb1ecfdd4d2bc8c1ec72f22d1dbe85f28a64e67
Ilnur Khusainov <khusainovilnur@gmail.com>
2019-09-17 13:44:40 +0500

    Initial commit

---
+++ src/main/java/project/khusainov/KhusainovApplication.java
@@ -0,0 +1,13 @@
+package project.khusainov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhusainovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhusainovApplication.class, args);
+	}
+
+}
